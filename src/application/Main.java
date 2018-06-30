ad45bcfa40544c8c72c62ab987d508d4ad9de4f2
okskm <okskm@DESKTOP-FNUMKI5>
2018-06-30 18:45:04 +0900

    레이아웃2차 수정

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("design.fxml"));
+		primaryStage.setTitle("지하철좌석알림시스템");
 		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
 	}
